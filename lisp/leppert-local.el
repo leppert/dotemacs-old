5a3c581711902c58730b25d80c124e21aa1e43a5
Greg Leppert <greg@leppert.me>
2015-06-27 15:32:05 -0400

    Save leppert-local customizations

--- lisp/leppert-local.el
+++ lisp/leppert-local.el
@@ -2,17 +2,37 @@
 
 ;;; Code:
 
+;; Config vars that shouldn't be checked into VCS
 (load "~/.emacs.d/leppert-local/private.el")
 
-;; Use two spaces for tabs in JS
-(setq-default js2-basic-offset 2)
-
-(setq c-default-style "k&r"
-      c-basic-offset 4)
+;; Load various custom functions
+(load "~/.emacs.d/leppert-local/functions.el")
 
 ;; tell emacs where to read abbrev definitions from
 (setq abbrev-file-name
       "~/.emacs.d/leppert-local/abbrev_defs")
+
+;; web-mode
+(defun web-mode-customizations ()
+  (setq web-mode-markup-indent-offset 2)
+)
+(add-hook 'web-mode-hook  'web-mode-customizations)
+
+;; Javascript / JS
+(setq-default js2-basic-offset 2)
+;; http://blog.binchen.org/posts/why-emacs-is-better-editor.html
+(add-hook 'js2-mode-hook 'js2-imenu-extras-mode)
+
+(setq c-default-style "k&r"
+      c-basic-offset 4)
+
+(add-hook 'js2-mode-hook (lambda () (tern-mode t)))
+
+(require 'company-tern)
+(eval-after-load 'company
+  '(add-to-list 'company-backends 'company-tern))
+
+;; Coffeescript
 
 (add-to-list 'load-path "~/.emacs.d/leppert-local/ac-coffee/")
 (require 'ac-coffee)
@@ -71,6 +91,8 @@
 
 ;; for the silver surfer highlighting
 (setq ag-highlight-search t)
+;; Bind search to super shift f
+(global-set-key (kbd "s-F") 'ag)
 
 ;; vim style kill line
 ;; via: http://stackoverflow.com/a/2173393/313561
@@ -86,6 +108,34 @@
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
+;; Python
+(elpy-enable)
+(load "~/.emacs.d/leppert-local/pony-mode.el")
+(define-key pony-minor-mode-map (kbd "C-c C-c") 'pony-test)
+
+;; Ruby
+(require 'rvm)
+(rvm-use-default)
+
+(require 'rinari)
+(define-key rinari-minor-mode-map (kbd "C-c C-c") 'rinari-test)
+
+(require 'rspec-mode)
+(define-key rspec-mode-map (kbd "C-c C-c") 'rspec-verify-single)
+
+;; PHP
+(require 'php-mode)
+(add-to-list 'auto-mode-alist '("\\.php\\'" . php-mode))
+
+;; Vagrant TRAMP
+(eval-after-load 'tramp
+  '(vagrant-tramp-enable))
+
+;; In order to use the faster Magit "next" branch
+;; TODO remove once Magit "next" is released
+(add-to-list 'load-path "~/.emacs.d/leppert-local/magit/")
+(require 'magit)
 
 (provide 'leppert-local)
 ;;; leppert-local.el ends here
